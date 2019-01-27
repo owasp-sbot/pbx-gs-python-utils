32149806915e7329f4ea8b38b0ee718d53a9ec9b
Dinis Cruz <dinis.cruz@owasp.org>
2019-01-27 18:08:17 +0000

    Added create shapes methods to GSlides

--- src/gsuite/GSlides.py
+++ src/gsuite/GSlides.py
@@ -13,7 +13,7 @@ class GSlides:
     # misc utils
 
     def batch_update(self, file_id, requests):
-        body = {'requests': [ requests ] }
+        body = {'requests': requests  }
         return self.execute(self.presentations.batchUpdate(presentationId=file_id, body=body))
 
     def execute(self,command):
@@ -29,62 +29,74 @@ class GSlides:
 
 
     # Elements
+    def element_create_image(self, file_id, page_id, image_url, x_pos=200, y_pos=200, width=100, height=100):
+        requests = [ {  "createImage": {
+                        "url"        : image_url,
+                        "elementProperties": {
+                            "pageObjectId": page_id,
+                            "size": { "width" : { "magnitude": width, "unit": "PT" },
+                                      "height": { "magnitude": height,"unit": "PT" }},
+                            "transform": { "scaleX": 1, "scaleY": 1, "translateX": x_pos, "translateY": y_pos, "unit": "PT" }}}}]
+        result = self.batch_update(file_id, requests)
+        return result.get('replies')[0].get('createImage').get('objectId')
 
-    def element_create_text(self,file_id, page_id, text, x_pos, y_pos, width, height):
-
+    def element_create_text(self,file_id, page_id, text = "Text...", x_pos=200, y_pos=200, width=100, height=100):
         element_id = self.random_id('Textbox')
-        requests = [
-            {
-                'createShape': {
-                    'objectId': element_id,
-                    'shapeType': 'TEXT_BOX',
-                    'elementProperties': {
-                        'pageObjectId': page_id,
-                        'size': {
-                            'height': { 'magnitude': height, 'unit': 'PT'},
-                            'width' : { 'magnitude': width , 'unit': 'PT'}
-                        },
-                        'transform': {
-                            'scaleX': 1,
-                            'scaleY': 1,
-                            'translateX': x_pos,
-                            'translateY': y_pos,
-                            'unit': 'PT'
-                        }
-                    }
-                }
-            },
-            {
-                'insertText': {
-                    'objectId': element_id,
-                    'insertionIndex': 0,
-                    'text': text
-                }
-            }
-        ]
+        requests = [ { 'createShape': { 'objectId': element_id,
+                                        'shapeType': 'TEXT_BOX',
+                                        'elementProperties': {
+                                            'pageObjectId': page_id,
+                                            'size'        : { 'height': { 'magnitude': height, 'unit': 'PT'},
+                                                              'width' : { 'magnitude': width , 'unit': 'PT'}},
+                                            'transform'   : { 'scaleX': 1, 'scaleY': 1, 'translateX': x_pos, 'translateY': y_pos, 'unit': 'PT' }}}},
+                    { 'insertText': { 'objectId': element_id, 'insertionIndex': 0, 'text': text                                                  }}]
 
         result = self.batch_update(file_id, requests)
         return result.get('replies')[0].get('createShape').get('objectId')
-        #body = { 'requests' : requests}
 
-        #result = self.gdrive.execute(self.gslides.presentations.batchUpdate(presentationId=file_id, body=body))
+    def element_create_shape(self,file_id, page_id, shape_type, x_pos=200, y_pos=200, width=100, height=100):
+        requests = [ { 'createShape': { 'shapeType': shape_type,
+                                        'elementProperties': {
+                                            'pageObjectId': page_id,
+                                            'size'        : { 'height': { 'magnitude': height, 'unit': 'PT'},
+                                                              'width' : { 'magnitude': width , 'unit': 'PT'}},
+                                            'transform'   : { 'scaleX': 1, 'scaleY': 1, 'translateX': x_pos, 'translateY': y_pos, 'unit': 'PT' }}}}]
 
-
+        result = self.batch_update(file_id, requests)
+        if result:
+            return result.get('replies')[0].get('createShape').get('objectId')
 
     def element_delete(self, file_id, element_id):
-        requests = {  'deleteObject' : { 'objectId': element_id } }
+        requests = [ {  'deleteObject' : { 'objectId': element_id } } ]
         return self.batch_update(file_id, requests)
 
     def element_set_text(self, file_id, element_id, text):
 
-        body = { 'requests' : [    {   'deleteText' : { 'objectId'      : element_id         ,
-                                                        'textRange'     : { 'type': 'ALL' }}},
-                                   {
-                                       'insertText': { 'objectId'      : element_id          ,
-                                                       'insertionIndex': 0                   ,
-                                                       'text'          : text             }}]}
+        requests =  [    {   'deleteText' : { 'objectId'      : element_id         ,
+                                              'textRange'     : { 'type': 'ALL' }}},
+                         {
+                             'insertText': { 'objectId'      : element_id          ,
+                                             'insertionIndex': 0                   ,
+                                             'text'          : text             }}]
 
-        return self.gdrive.execute(self.presentations.batchUpdate(presentationId=file_id, body=body))
+        return self.batch_update(file_id, requests)
+
+    def element_set_text_style(self, file_id, shape_id,style, fields):
+        requests = [{'updateTextStyle': { 'objectId': shape_id  ,
+                                          'style'   : style     ,
+                                          'fields'  : fields  }}]
+        return self.batch_update(file_id, requests)
+
+    def element_set_shape_properties(self, file_id, shape_id,properties , fields=None):
+        if fields is None:
+            fields = ",".join(list(set(properties)))
+        requests = [{'updateShapeProperties': { 'objectId'         : shape_id     ,
+                                                'shapeProperties'  : properties   ,
+                                                'fields'           : fields     }}]
+        return self.batch_update(file_id, requests)
+
+    def element_set_via_requests(self, file_id, requests):
+        return self.batch_update(file_id, requests)
 
 
     def presentation_create(self, title):
--- test/gsuite/Test_GSlides.py
+++ test/gsuite/Test_GSlides.py
@@ -1,3 +1,4 @@
+from time import sleep
 from unittest        import TestCase
 
 from gsuite.GDrive    import GDrive
@@ -11,6 +12,15 @@ class Test_GDrive(TestCase):
         self.gslides = GSlides()
         self.gdrive  = GDrive()
         self.test_id = '1CA-uqZj9HVr2_RHiI-esVyHBoHZ1M1sxGzq54EQ2Ek4'
+
+    # helper methods
+
+    def get_target_slide_id(self):
+        file_id  = self.gdrive.find_by_name('GSlides API tests').get('id')
+        slides   = self.gslides.slides(self.test_id)
+        slide_id = slides.pop().get('objectId')
+        return file_id, slide_id
+    # tests for GDrive methods
 
     def test_ctor(self):
         service = self.gslides.service
@@ -55,24 +65,121 @@ class Test_GDrive(TestCase):
         assert result.get('presentationId') == file_id
 
 
-    def test_delete_element(self):
+    def test_delete_element__all_temp_created(self):
         file_id  = self.gdrive.find_by_name('GSlides API tests').get('id')
         elements = self.gslides.slide_elements_indexed_by_id(file_id,1)
         for key,element in elements.items():
-            Dev.pprint(key)
+            #Dev.pprint(key)
             if 'textbox_' in key.lower():
                 Dev.pprint(self.gslides.element_delete(file_id,key))
 
 
 
     def test_add_element_text(self):
-        file_id = self.gdrive.find_by_name('GSlides API tests').get('id')
-        slides  = self.gslides.slides(self.test_id)
-        page_id = slides.pop().get('objectId')
+        (file_id, slide_id) = self.get_target_slide_id()
         text    = Misc.random_string_and_numbers(6,'New Text Field - ')
         x_pos   = 50
         y_pos   = 250
         width   = 300
         height  = 50
-        result = self.gslides.element_create_text(file_id, page_id, text, x_pos, y_pos, width, height)
-        Dev.pprint(result)
+        new_id = self.gslides.element_create_text(file_id, slide_id, text, x_pos, y_pos, width, height)
+
+        self.gslides.element_set_text(file_id,new_id,'changed text')
+        sleep(2)
+        self.gslides.element_delete(file_id,new_id)
+
+    def test_element_create_image(self):
+        (file_id, slide_id) = self.get_target_slide_id()
+        image_url = 'https://pbx-group-security.com/img/pbx-gs/pbx-gs-logo.png'
+        shape_id  = self.gslides.element_create_image(file_id, slide_id,image_url, 100,100,300,300)
+        sleep(3)
+        self.gslides.element_delete(file_id, shape_id)
+
+    def test_element_create_shape(self):
+        (file_id, slide_id) = self.get_target_slide_id()
+        def test_shape_creation(shape_type):
+            shape_id  = self.gslides.element_create_shape(file_id, slide_id, shape_type,225,50,300,300)
+            #properties = {"shapeBackgroundFill": {"solidFill": {"alpha": 0.6, "color": {"themeColor": "ACCENT5"}}}}
+            #self.gslides.element_set_shape_properties(file_id, shape_id, properties)
+            if shape_id:
+                self.gslides.element_delete(file_id, shape_id)
+
+        # enabling all 140 shapes below will cause an "Quota exceeded for quota group 'WriteGroup' and limit 'USER-100s" error
+
+        shapes = [  #'TEXT_BOX','RECTANGLE','ROUND_RECTANGLE','ELLIPSE','ARC','BENT_ARROW','BENT_UP_ARROW','BEVEL',
+                    #'BLOCK_ARC','BRACE_PAIR','BRACKET_PAIR','CAN','CHEVRON','CHORD','CLOUD','CORNER','CUBE',
+                    #'CURVED_DOWN_ARROW','CURVED_LEFT_ARROW','CURVED_RIGHT_ARROW','CURVED_UP_ARROW','DECAGON',
+                    #'DIAGONAL_STRIPE','DIAMOND','DODECAGON','DONUT','DOUBLE_WAVE','DOWN_ARROW','DOWN_ARROW_CALLOUT',
+                    #'FOLDED_CORNER','FRAME','HALF_FRAME','HEART','HEPTAGON','HEXAGON','HOME_PLATE','HORIZONTAL_SCROLL',
+                    #'IRREGULAR_SEAL_1','IRREGULAR_SEAL_2','LEFT_ARROW','LEFT_ARROW_CALLOUT','LEFT_BRACE','LEFT_BRACKET',
+                    #'LEFT_RIGHT_ARROW','LEFT_RIGHT_ARROW_CALLOUT','LEFT_RIGHT_UP_ARROW','LEFT_UP_ARROW','LIGHTNING_BOLT',
+                    #'MATH_DIVIDE','MATH_EQUAL','MATH_MINUS','MATH_MULTIPLY','MATH_NOT_EQUAL','MATH_PLUS','MOON',
+                    #'NO_SMOKING','NOTCHED_RIGHT_ARROW','OCTAGON','PARALLELOGRAM','PENTAGON','PIE','PLAQUE','PLUS',
+                    #'QUAD_ARROW','QUAD_ARROW_CALLOUT','RIBBON','RIBBON_2','RIGHT_ARROW','RIGHT_ARROW_CALLOUT',
+                    #'RIGHT_BRACE','RIGHT_BRACKET','ROUND_1_RECTANGLE','ROUND_2_DIAGONAL_RECTANGLE','ROUND_2_SAME_RECTANGLE',
+                    #'RIGHT_TRIANGLE','SMILEY_FACE','SNIP_1_RECTANGLE','SNIP_2_DIAGONAL_RECTANGLE','SNIP_2_SAME_RECTANGLE',
+                    #'SNIP_ROUND_RECTANGLE','STAR_10','STAR_12','STAR_16','STAR_24','STAR_32','STAR_4','STAR_5','STAR_6',
+                    #'STAR_7','STAR_8','STRIPED_RIGHT_ARROW','SUN','TRAPEZOID','TRIANGLE','UP_ARROW','UP_ARROW_CALLOUT',
+                    'UP_DOWN_ARROW','UTURN_ARROW','VERTICAL_SCROLL','WAVE','WEDGE_ELLIPSE_CALLOUT','WEDGE_RECTANGLE_CALLOUT',
+                    'WEDGE_ROUND_RECTANGLE_CALLOUT','FLOW_CHART_ALTERNATE_PROCESS','FLOW_CHART_COLLATE','FLOW_CHART_CONNECTOR',
+                    'FLOW_CHART_DECISION','FLOW_CHART_DELAY','FLOW_CHART_DISPLAY','FLOW_CHART_DOCUMENT','FLOW_CHART_EXTRACT',
+                    'FLOW_CHART_INPUT_OUTPUT','FLOW_CHART_INTERNAL_STORAGE','FLOW_CHART_MAGNETIC_DISK','FLOW_CHART_MAGNETIC_DRUM',
+                    'FLOW_CHART_MAGNETIC_TAPE','FLOW_CHART_MANUAL_INPUT','FLOW_CHART_MANUAL_OPERATION','FLOW_CHART_MERGE',
+                    'FLOW_CHART_MULTIDOCUMENT','FLOW_CHART_OFFLINE_STORAGE','FLOW_CHART_OFFPAGE_CONNECTOR','FLOW_CHART_ONLINE_STORAGE',
+                    'FLOW_CHART_OR','FLOW_CHART_PREDEFINED_PROCESS','FLOW_CHART_PREPARATION','FLOW_CHART_PROCESS','FLOW_CHART_PUNCHED_CARD',
+                    'FLOW_CHART_PUNCHED_TAPE','FLOW_CHART_SORT','FLOW_CHART_SUMMING_JUNCTION','FLOW_CHART_TERMINATOR','ARROW_EAST',
+                    'ARROW_NORTH_EAST','ARROW_NORTH','SPEECH','STARBURST','TEARDROPELLIPSE_RIBBON','ELLIPSE_RIBBON_2','CLOUD_CALLOUT']
+        for shape in shapes:
+            test_shape_creation(shape)
+
+
+
+    def test_element_set_text_style(self):
+        (file_id, slide_id) = self.get_target_slide_id()
+        shape_id = self.gslides.element_create_text(file_id, slide_id,width=400)
+
+        style = {  'bold'            : True,
+                   'italic'          : True,
+                   'fontFamily'      : 'Times New Roman',
+                   'fontSize'        : { 'magnitude'  : 74, 'unit': 'PT' },
+                   'foregroundColor' : { 'opaqueColor': { 'rgbColor': { 'blue': 0.5, 'green': 1.0, 'red': 0.0 }}},
+                    'link'           : { 'url': 'https://news.bbc.co.uk'         } }
+        fields   = 'bold,italic,fontFamily,fontSize, foregroundColor,link'
+
+        self.gslides.element_set_text_style(file_id,shape_id, style, fields)
+        sleep(3)
+        self.gslides.element_delete(file_id,shape_id)
+
+    def test_element_set_shape_properties(self):
+        (file_id, slide_id) = self.get_target_slide_id()
+        shape_id = self.gslides.element_create_text(file_id, slide_id,width=400)
+
+        properties = { "shapeBackgroundFill": { "solidFill"  : { "alpha": 0.6, "color": { "themeColor": "ACCENT5" } } },
+                        "outline"           : { "dashStyle"  : "SOLID",
+                                                "outlineFill": { "solidFill": { "alpha": 1, "color": { "themeColor": "ACCENT5" }} },
+                                                "weight"     : {"magnitude": 3, "unit": "PT"                                    } },
+                        "contentAlignment"  : 'MIDDLE'                                                                            }
+
+        fields = "shapeBackgroundFill,outline, contentAlignment"
+
+        self.gslides.element_set_shape_properties(file_id,shape_id,properties,fields)
+        sleep(3)
+        self.gslides.element_delete(file_id, shape_id)
+
+    def test_element_set_via_requests(self):
+        (file_id, slide_id) = self.get_target_slide_id()
+        shape_id = self.gslides.element_create_text(file_id, slide_id, width=400)
+        requests = []
+        requests.append  ({ 'updateTextStyle'       : { 'objectId'       : shape_id  ,
+                                                        'style'          : { 'fontSize': {'magnitude': 74, 'unit': 'PT'}},
+                                                        'fields'         : 'fontSize'                                  }})
+        requests.append  ({ 'updateParagraphStyle'  : { "objectId"       : shape_id,
+                                                        "style"          : { "alignment": "CENTER" },
+                                                        "fields"         : 'alignment'                                 }})
+        requests.append  ({ "updateShapeProperties" : { "objectId"       : shape_id,
+                                                        'shapeProperties': { "shapeBackgroundFill" : { "solidFill"  : { "alpha": 0.6, "color": { "themeColor": "ACCENT5" } } }},
+                                                        'fields'         : 'shapeBackgroundFill'                       }})
+
+        self.gslides.element_set_via_requests(file_id, requests)
+        sleep(2)
+        self.gslides.element_delete(file_id, shape_id)
