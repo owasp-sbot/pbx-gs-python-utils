489dddc939a5c68520bcbde25d6ce53e5e4f5865
Dinis Cruz <dinis.cruz@owasp.org>
2019-03-02 10:15:53 +0000

    Added ability to execute arrays of js code

--- src/browser/API_Browser.py
+++ src/browser/API_Browser.py
@@ -49,14 +49,23 @@ class API_Browser:
 
     async def js_execute(self, js_code):
         if js_code:
-            if type(js_code).__name__ == 'str':
+            if type(js_code).__name__ == 'str':                             # if it is a string, execute it
                 return await self.js_eval(js_code)
-            else:
-                name   = js_code.get('name')
-                params = js_code.get('params'  )
-                return await self.js_invoke_function(name, params)
-            #from time import sleep                                 # we might need to add some kind of timeout or callback (to handle cases when actions need a bit more time to stabilize after the js execution)
-            #sleep(0.250)
+
+            if type(js_code).__name__ == 'list':                            # if it is an list (i.e. array)
+                list_types = [type(item).__name__ for item in js_code]      # get all array items type
+                all_string = list(set(list_types)) == ['str']               # get unique list and check if all are string
+                if all_string:                                              # if they are
+                    js_code = ";\n".join(js_code)                           # join them all (separated by ;)
+                    return await self.js_eval(js_code)                      # execute it
+
+            name   = js_code.get('name')                                    # if js_code was an object
+            params = js_code.get('params'  )                                # get the name and params values
+            if name and params:
+                return await self.js_invoke_function(name, params)          # execute them as a js method
+
+            #from time import sleep                                         # we might need to add some kind of timeout or callback (to handle cases when actions need a bit more time to stabilize after the js execution)
+            #sleep(0.250)                                                   # but I think this is better done outside this function
 
     async def js_eval(self, code):
         page = await self.page()
