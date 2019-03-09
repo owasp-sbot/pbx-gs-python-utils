31d7060d192026b7b1b490cbc698463edcdc17bd
Dinis Cruz <dinis.cruz@owasp.org>
2019-03-09 14:09:44 +0000

    Added API_Jira

--- src/utils/Http.py
+++ src/utils/Http.py
@@ -41,3 +41,35 @@ def port_is_open(port, host='0.0.0.0'):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     result = sock.connect_ex((host, port))
     return result == 0
+
+
+
+# code below is not reliable enough (was trying to get the time from an ntp server, but it hanged a couple times during tests)
+# note: this could had been caused by VPN issues (I was having at the time with the Cisco VPN client)
+# def gettime_ntp(self, ntp_server='0.uk.pool.ntp.org'):
+#     #ntp_server = 'europe.pool.ntp.org'
+#     # based on http://code.activestate.com/recipes/117211-simple-very-sntp-client/
+#     import struct
+#     import sys
+#     import time
+#
+#     TIME1970 = 2208988800  # Thanks to F.Lundh
+#
+#     from _socket import AF_INET
+#     from _socket import SOCK_DGRAM
+#     from socket import socket
+#     client = socket(AF_INET, SOCK_DGRAM)
+#     data = '\x1b' + 47 * '\0'
+#     client.sendto(data.encode(), (ntp_server, 123))
+#     client.settimeout(2)
+#     data, address = client.recvfrom(1024)
+#     if data:
+#         print('Response received from:', address)
+#         t = struct.unpack('!12I', data)[10]
+#         t -= TIME1970
+#         date = time.ctime(t)
+#
+#
+#         when = strftime("%Y/%m/%d %H:%M", localtime(t))
+#         print(when)
+#         print('\tTime=%s' % time.ctime(t))
