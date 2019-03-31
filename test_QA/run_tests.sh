841d5d59b4ac387ee81f14b0fee9edf88c35b88f
Dinis Cruz <dinis.cruz@owasp.org>
2019-03-31 20:47:06 +0100

    minor fix

--- test_QA/run_tests.sh
+++ test_QA/run_tests.sh
@@ -5,5 +5,5 @@ echo "*****************************"
 echo "*** Running tests         ***"
 echo "*****************************"
 
-#pytest -s -v
-pytest -s -v test_Update_Lambda_Functions.py
+pytest -s -v
+#pytest -s -v test_Update_Lambda_Functions.py
