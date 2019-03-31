02a9887559522d327b58a301e5fb8eedcd5060d1
Dinis Cruz <dinis.cruz@owasp.org>
2019-03-31 21:04:34 +0100

    minor fix

--- pbx_gs_python_utils/utils/Lambdas_Helpers.py
+++ pbx_gs_python_utils/utils/Lambdas_Helpers.py
@@ -14,7 +14,7 @@ def log_to_elk(message, data = None, index = "gs_bot_logs", level = "debug", cat
 
     Lambdas('utils_log_to_elk').invoke_async(payload)
 
-def slack_message(text, attachments = [], channel = 'GBMGMK88Z', team_id='T7F3AUXGV'):  # GBMGMK88Z is the 'from-aws-lambda' channel in the GS-CST Slack workspace
+def slack_message(text, attachments = [], channel = 'GDL2EC3EE', team_id='T7F3AUXGV'):  # GBMGMK88Z is the 'from-aws-lambda' channel in the GS-CST Slack workspace
     payload = {
                 'text'        : text        ,
                 'attachments' : attachments ,
