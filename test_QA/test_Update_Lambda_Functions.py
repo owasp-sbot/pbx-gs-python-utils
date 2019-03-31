948cfaf5130baacaf37aed229cf721901c194c70
Dinis Cruz <dinis.cruz@owasp.org>
2019-03-31 20:57:11 +0100

    minor fix

--- test_QA/test_Update_Lambda_Functions.py
+++ test_QA/test_Update_Lambda_Functions.py
@@ -11,13 +11,15 @@ class test_Update_Lambda_Functions(TestCase):
         self.update = Update_Lambda_Functions()
 
     def test_update_lambda_functions(self):
-        Dev.pprint('in test_update_lambda_functions')
+        Dev.pprint(':zero: in test_update_lambda_functions')
         result = self.update.update_lambda_functions()
 
         Dev.pprint(result)
         text = ":building_construction: pbx_gs_python_utils.update_lambda_functions:"
         attachments = [ { 'text': json.dumps(result,indent=4) , 'color':'good'}]
         slack_message(text,attachments,'GDL2EC3EE')  #gs-bot-tests
+
+        Dev.pprint(':one: finished test_update_lambda_functions')
         #Dev.pprint(result)
 
     #
