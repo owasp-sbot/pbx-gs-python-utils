b6ba329d375383e23552208bb4cbb2fa76baf398
Dinis Cruz <dinis.cruz@owasp.org>
2019-03-31 20:55:28 +0100

    minor fix

--- test_QA/run_tests.sh
+++ test_QA/run_tests.sh
@@ -5,5 +5,6 @@ echo "*****************************"
 echo "*** Running tests         ***"
 echo "*****************************"
 
-pytest -s -v
+#pytest -s -v
+pytest -s -v --ignore utils/slack
 #pytest -s -v test_Update_Lambda_Functions.py
