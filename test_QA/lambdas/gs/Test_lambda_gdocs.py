fe3015c94b7e7e6eace5dcfa60062c2b5bbb2853
Dinis Cruz <dinis.cruz@owasp.org>
2019-04-01 14:21:37 +0100

    minor fix

--- test_QA/lambdas/gs/Test_lambda_gdocs.py
+++ test_QA/lambdas/gs/Test_lambda_gdocs.py
@@ -8,9 +8,9 @@ class Test_Lambda_lambda_gdocs(TestCase):
     def setUp(self):
         self.lambda_gdocs = Lambdas('pbx_gs_python_utils.lambdas.gs.lambda_gdocs', memory=3008)
 
-    def test_update(self):
-        self.lambda_gdocs.update_with_src()
+    #def test_update(self):
+    #    self.lambda_gdocs.update_with_src()
 
-    def test_update_invoke(self):
+    def test_invoke(self):
         result = self.lambda_gdocs.update_with_src().invoke({ 'data':{}, 'params':['pdf','1xIeV2eQb59EsiJoOUB1yOK3FY2LCvzMmTgvhAVXlEEI']})
         Dev.pprint(result)
