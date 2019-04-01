641bc46c03e5f914b8a38b0bc65fb41b91a2b593
Dinis Cruz <dinis.cruz@owasp.org>
2019-04-01 21:47:06 +0100

    added GS_Bot_Commands tests

--- pbx_gs_python_utils/gsbot/GS_Bot_Commands.py
+++ pbx_gs_python_utils/gsbot/GS_Bot_Commands.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from pbx_gs_python_utils.utils.Lambdas_Helpers       import slack_message
 from pbx_gs_python_utils.utils.aws.Lambdas           import Lambdas
 
-gsbot_version = 'v0.54'
+gsbot_version = 'v0.55'
 
 class GS_Bot_Commands:                                      # move to separate class
     @staticmethod
@@ -130,7 +130,7 @@ class GS_Bot_Commands:                                      # move to separate c
 
     @staticmethod
     def version(slack_event, params=[]):
-        return 'The current version of GSBot is {0}'.format(gsbot_version),None
+        return 'The current version of GSBot is {0}'.format(gsbot_version),[]
 
 
     # @staticmethod
