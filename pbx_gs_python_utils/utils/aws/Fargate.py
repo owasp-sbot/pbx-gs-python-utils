9d6d5ff34bacf18c73b593b5ac19b5477f7f3f3c
Dinis Cruz <dinis.cruz@owasp.org>
2019-04-06 01:15:51 +0100

    Added just one test to buildspec.py

--- pbx_gs_python_utils/utils/aws/Fargate.py
+++ pbx_gs_python_utils/utils/aws/Fargate.py
@@ -7,7 +7,10 @@ class Fargate:
         self.ecs = boto3.client('ecs')
 
     def cluster_delete(self, cluster_arn):
-        self.ecs.delete_cluster(cluster_arn)
+        return self.ecs.delete_cluster(cluster=cluster_arn)
 
     def clusters(self):
         return self.ecs.list_clusters().get('clusterArns')
+
+    def task_definitions(self):
+        return self.ecs.list_task_definitions().get('taskDefinitionArns')
--- test_QA/utils/aws/test_Fargate.py
+++ test_QA/utils/aws/test_Fargate.py
@@ -1,3 +1,5 @@
+import sys ; sys.path.append('..')
+
 from unittest import TestCase
 
 from pbx_gs_python_utils.utils.Dev import Dev
@@ -12,12 +14,17 @@ class test_Fargate(TestCase):
     def test__init__(self):
         assert type(self.fargate.ecs).__name__ == 'ECS'
 
-    def test_cluster_delete(self):
-        clusters_arns = self.fargate.clusters()
-        Dev.pprint(clusters_arns)
-        #clusterArns' in set(result)
+    # def test_cluster_delete(self):
+    #     clusters_arns = self.fargate.clusters()
+    #     result = self.fargate.cluster_delete(clusters_arns[0])
+    #     Dev.pprint(result)
 
     def test_clusters(self):
         result = self.fargate.clusters()
         if len(result) > 0:
             assert 'arn:aws:ecs:' in result[0]
+
+    def test_task_definitions(self):
+        result = self.fargate.task_definitions()
+        assert len(result) > 1
+        assert ':task-definition/' in result[0]
