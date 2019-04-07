4d2dcb0058556878173fca23ecadcf85e755d58e
Dinis Cruz <dinis.cruz@owasp.org>
2019-04-07 10:04:41 +0100

    Added more AWS API helpers

--- pbx_gs_python_utils/utils/aws/Cloud_Watch.py
+++ pbx_gs_python_utils/utils/aws/Cloud_Watch.py
@@ -1,44 +1,6 @@
 import boto3
 
-from pbx_gs_python_utils.utils.Dev import Dev
-
-
-class Cloud_Watch:
+class Cloud_Watch():
     def __init__(self):
         self.cloudwatch = boto3.client('cloudwatch')
-        self.logs       = boto3.client('logs')
 
-    def _invoke_via_paginator(self, api, method, field_id, **kwargs):
-        paginator = api.get_paginator(method)
-        for page in paginator.paginate(**kwargs):
-            for id in page.get(field_id):
-                yield id
-
-    def log_group_create(self, name):
-        self.logs.create_log_group(logGroupName=name)
-        return self
-
-    def log_group_delete(self, name):
-        self.logs.delete_log_group(logGroupName=name)
-        return self
-
-    def log_group_exists(self, name):
-        return self.log_group_details(name) is not None
-
-    def log_group_details(self, name):
-        for log_group in self.logs.describe_log_groups(logGroupNamePrefix= name).get('logGroups'):
-            if log_group.get('logGroupName') == name:
-                return log_group
-        return None
-
-    def log_groups(self):
-        groups = {}
-        for group in  self._invoke_via_paginator(self.logs, 'describe_log_groups', 'logGroups'): # self.logs.describe_log_groups().get('logGroups'):
-            groups[group.get('logGroupName')] = group
-        return groups
-
-    def logs_get_messages(self,group_name, stream_name):
-        messages = []
-        for event in self.logs.get_log_events(logGroupName=group_name, logStreamName=stream_name).get('events'):
-            messages.append(event.get('message'))
-        return messages
--- test_QA/utils/aws/test_CloudWatch.py
+++ test_QA/utils/aws/test_CloudWatch.py
@@ -12,20 +12,3 @@ class test_Cloud_Watch(TestCase):
     def test__init__(self):
         type(self.cloud_watch.cloudwatch).__name__ == 'CloudWatch'
 
-    def test_log_group_create(self):
-        name    = 'test_log_group'
-        assert self.cloud_watch.log_group_exists(name) is False
-        self.cloud_watch.log_group_create(name)
-        assert self.cloud_watch.log_group_exists(name) is True
-        assert self.cloud_watch.log_group_details(name).get('logGroupName') == name
-        self.cloud_watch.log_group_delete(name)
-        assert self.cloud_watch.log_group_exists(name) is False
-
-    def test_log_groups(self):
-        assert len(list(self.cloud_watch.log_groups())) > 1
-
-    def test_logs(self):
-        group_name  = 'awslogs-temp_task_on_temp_cluster_X29B3K'
-        stream_name = 'awslogs-example/gs-docker-codebuild/f8ccf213-b642-466c-8458-86af9933eca9'
-        messages    = self.cloud_watch.logs_get_messages(group_name, stream_name)
-        Dev.pprint(messages)
