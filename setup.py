09dadf899adf6c4c4565ea51dcd6a51a8cd9e427
Felipe Zipitria <fzipitria@perceptyx.com>
2019-05-11 09:20:03 -0300

    Add slackclient package dependency

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setuptools.setup(
     long_description              = long_description,
     long_description_content_type = " text/markdown",
     url                           = "https://github.com/pbx-gs/pbx-gs-python-utils",
+    install_requires              = [
+                                      'slackclient',
+                                  ],
     packages                      = setuptools.find_packages(),
     classifiers                   = [ "Programming Language :: Python :: 3"   ,
                                       "License :: OSI Approved :: MIT License",
