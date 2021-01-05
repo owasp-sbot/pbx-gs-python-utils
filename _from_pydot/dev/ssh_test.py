716892cf75d05b996fec3626debce10c4d10c0e0
Dinis Cruz <dinis.cruz@owasp.org>
2021-01-05 20:57:32 +0000

    added _from_pydot files (lots of interesting code and experiments in there)

---
+++ _from_pydot/dev/ssh_test.py
@@ -0,0 +1,8 @@
+from osbot_utils.utils.Process import Process
+
+def an_method(params):
+    return Process.run('echo', [params])
+
+def run(event, context):
+    an_method('this is an method')
+    return "ssh test..."
